f8c7555bd14861ba64296e1ea6c23d2ae40f86b4
Luka Fontanilla <51471651+LukaFontanilla@users.noreply.github.com>
2022-06-29 15:35:46 -0700

    test

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -19,7 +19,7 @@ export const Nav: React.FC = () => {
                 <Link href="/art" passHref><p className="footerRowText">Art</p></Link>
                 <Link href="/projects" passHref><p className="footerRowText">Project Plans</p></Link>
                 <div className={styles.footerRowTextIcon} onClick={darkMode.value ? darkMode.disable : darkMode.enable}>
-                {!isMounted || navigator?.connection?.saveData || !matchMedia('(min-width: 768px)').matches ? null : (
+                {!isMounted || !matchMedia('(min-width: 768px)').matches ? null : (
                 <Suspense fallback={null}>
                     <Spheres />
                 </Suspense>
